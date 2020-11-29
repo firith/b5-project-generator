55e34ebe92b44708bb7362c79a23612e316ac655
Simon Balázs <balazs.simon@intren.hu>
2020-11-29 10:12:25 +0100

    Add framework settings

--- app/Commands/InitCommand.php
+++ app/Commands/InitCommand.php
@@ -42,26 +42,43 @@ class InitCommand extends Command
      */
     public function handle()
     {
-        $this->appName = (string) $this->argument('app-name');
+        $this->appName = (string)$this->argument('app-name');
         $this->parentDirectory = $this->argument('app-name') . '-b5';
 
         if ($this->option('framework') && !in_array($this->option('framework'), $this->frameworks)) {
             throw new InvalidArgumentException(sprintf('Támogatott frameworkök: %s', implode(', ', $this->frameworks)));
         }
 
-        $this->task('Generate b5 files', function () {
-            Storage::disk('cwd')->makeDirectory($this->parentDirectory . '/build');
-            Storage::disk('cwd')->makeDirectory($this->parentDirectory . '/' . $this->argument('app-name'));
+        $this->task('Generate b5 files', fn() => $this->generateB5Files());
 
-            $this->generateFile('config.yml', 'config.yml');
-            $this->generateFile('Taskfile', 'Taskfile');
-            $this->generateFile('docker-compose.yaml', 'docker-compose.yaml');
-            $this->generateFile('configs/nginx/site.conf', 'configs/nginx/site.conf');
-            $this->generateFile('configs/php/Dockerfile', 'configs/php/Dockerfile');
+        if ($this->option('framework') === 'laravel') {
+            $this->task('Generate laravel specific b5 files', function () {
+                $this->generateFile('config-laravel.yml', 'config.yml');
+                $this->generateFile('Taskfile-laravel', 'Taskfile');
+                $this->generateFile('docker-compose-laravel.yaml', 'docker-compose.yaml');
+                $this->generateFile('configs/nginx/site-laravel.conf', 'configs/nginx/site.conf');
+                $this->generateFile('configs/php/Dockerfile-laravel', 'configs/php/Dockerfile');
+            });
+        }
+
+        if ($this->option('framework') === 'symfony') {
+            $this->task('Generate laravel specific b5 files', function () {
+                $this->generateFile('config-symfony.yml', 'config.yml');
+                $this->generateFile('Taskfile-symfony', 'Taskfile');
+                $this->generateFile('docker-compose-symfony.yaml', 'docker-compose.yaml');
+                $this->generateFile('configs/nginx/site-symfony.conf', 'configs/nginx/site.conf');
+                $this->generateFile('configs/php/Dockerfile-symfony', 'configs/php/Dockerfile');
+            });
+        }
+
+        $this->task('Installing framework', function () {
+
+        });
+
+        $this->task('Initialize git', function () {
             // Todo gitignore és gitkeep
             //todo git init
 
-            Storage::disk('cwd')->put($this->parentDirectory . '/'. $this->appName .'/index.php', 'hello world');
         });
 
         $this->task('Docker build', function () {
@@ -77,5 +94,21 @@ class InitCommand extends Command
         $fileContent = Str::of($fileContent)
             ->replace('{{ appName }}', $this->appName);
         Storage::disk('cwd')->put($this->parentDirectory . '/build/' . $path, $fileContent);
+    }
+
+    private function generateB5Files(): void
+    {
+        Storage::disk('cwd')->makeDirectory($this->parentDirectory . '/build');
+        Storage::disk('cwd')->makeDirectory($this->parentDirectory . '/' . $this->argument('app-name'));
+
+        $this->generateFile('config.yml', 'config.yml');
+        $this->generateFile('Taskfile', 'Taskfile');
+        $this->generateFile('docker-compose.yaml', 'docker-compose.yaml');
+        $this->generateFile('configs/nginx/site.conf', 'configs/nginx/site.conf');
+        $this->generateFile('configs/php/Dockerfile', 'configs/php/Dockerfile');
+
+        if (!$this->option('framework')) {
+            Storage::disk('cwd')->put($this->parentDirectory . '/' . $this->appName . '/index.php', 'hello world');
+        }
     }
 }
